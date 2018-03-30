1aa91fca1a257ebd86b8139a086204bf901b39d1
User <zhan0530@algonquinlive.com>
2018-03-30 18:19:19 -0400

    OCTRANSPO

--- app/src/main/java/com/example/shican/quizcreator/OCAdd.java
+++ app/src/main/java/com/example/shican/quizcreator/OCAdd.java
@@ -15,25 +15,22 @@ import android.widget.Toast;
 
 public class OCAdd extends AppCompatActivity {
     protected static final String ACTIVITY_NAME = "OCAdd";
+    private static int result = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_oc_delete);
+        setContentView(R.layout.activity_oc_add);
 
         final Button submit = (Button) findViewById(R.id.submit);
         submit.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
                 AlertDialog.Builder builder = new AlertDialog.Builder(OCAdd.this);
                 builder.setMessage("Do you want to add route?");
 
                 builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        Intent resultIntent = new Intent();
-                        resultIntent.putExtra("Response","Route Added");
-                        setResult(RESULT_OK, resultIntent);
-                        finish();
+                        result = 1;
                     }
                 });
 
@@ -42,6 +39,17 @@ public class OCAdd extends AppCompatActivity {
                     }
                 });
                 builder.show();
+            }
+        });
+
+        final Button back = (Button) findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent resultIntent = new Intent();
+                if(result == 1) resultIntent.putExtra("Response","Route Added");
+                if(result == 0) resultIntent.putExtra("Response","No Route Added");
+                setResult(RESULT_OK, resultIntent);
+                finish();
             }
         });
     }
