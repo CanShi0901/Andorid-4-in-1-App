c089b81c2e53a671f402948daa338a60a9d87812
User <zhan0530@algonquinlive.com>
2018-04-17 22:48:20 -0400

    documentation

--- app/src/main/java/com/example/shican/quizcreator/OCSavedRouteDatabaseHelper.java
+++ app/src/main/java/com/example/shican/quizcreator/OCSavedRouteDatabaseHelper.java
@@ -6,7 +6,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class OCSavedRouteDatabaseHelper extends SQLiteOpenHelper {
     public final static String DATABASE_NAME = "db";
-    public final static int VERSION_NUM = 8;
+    public final static int VERSION_NUM = 9;
     public final static String TABLE_NAME = "savedRouteTable";
     public final static String KEY_ID = "ID";
     public final static String KEY_MESSAGE = "Routes";
